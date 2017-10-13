cd3cd9899713a7dd7262abd6a188803f22c9fce8
Pablo <pabloatm980@gmail.com>
2017-10-13 13:34:24 -0300

    scenario

---
+++ automata/Scenario.py
@@ -0,0 +1,27 @@
+from Render import Render
+class Scenario():
+    def __init__(self,width, height):
+        self.width = width
+        self.height = height
+        self.current_moment = []
+        self.renderer = Render(self)
+
+
+    def __iter__(self):
+        return current_moment.__iter__()
+
+    def add_cell(self, cell):
+        self.current_moment.append(cell)
+
+    def atPoints(self,points):
+        return [c for c in self.current_moment for p in points if c.point.equal(p)]
+
+    def tick(self):
+        next_moment = []
+        for c in self.current_moment:
+            next_moment.append(c.action())
+
+        self.current_moment = next_moment
+
+    def __str__(self):
+        return 'Scenario'
