85581a0cf9742de42167475cabf2c26c50e3f1dc
Pablo <pabloatm980@gmail.com>
2017-10-13 13:34:05 -0300

    Automaton logic

---
+++ automata/Cell.py
@@ -0,0 +1,54 @@
+""" The base cell that acts in the scenario
+
+Has a point (place in the scenario),
+and a reference in said scenario
+
+"""
+import random
+
+class Ruleset():
+    def action(self):
+        pass
+
+class GameOfLife():
+    obj = None
+    def __init__(self):
+        pass
+
+    def action(self, cell, scenario):
+        neighborhood = scenario.atPoints(cell.point.vecinity(cell.radius))
+        nALives = len([n for n in neighborhood if n.alive])
+
+        if  nALives > 3 or nALives < 2:
+            cell.alive = False
+        elif nALives == 3:
+            cell.alive = True
+        else:
+            cell.alive = cell.alive
+
+class Cell():
+    def __init__(self, point, scenario, ruleset,radius=1):
+        self.radius = radius
+        self.point = point
+        self.scenario = scenario
+        self.ruleset = ruleset
+
+    def neighbors(self):
+        return self.point.vecinity(self.radius)
+
+    def action(self):
+        self.ruleset.action(self, self.scenario)
+        return self
+
+    def __str__(self):
+        return 'cell at {} {}'.format(self.point.x, self.point.y)
+
+class GoLCell(Cell):
+    def __init__(self, point, scenario, radius, ruleset=GameOfLife()):
+        self.alive = random.random() < 0.3
+
+        super(GoLCell,self).__init__(point, scenario,ruleset, radius)
+
+    def __str__(self):
+        return 'cell at {} {}'.format(self.point.x, self.point.y)
+
