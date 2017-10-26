4310efb951804b2b981385a1fd9f981084c155e1
Pablo <pabloatm980@gmail.com>
2017-10-25 23:13:23 -0300

    anda con los nuevos cambios

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from automata.ruleset.GameOfLifeRuleset import GameOfLife
 from automata.Point import Point
 from automata.ruleset.Ruleset import Ruleset
 from automata.ruleset.Handstander import Handstander
+from automata.ruleset.TupperWasher import TupperWasher
+from automata.ruleset.StoneThrower import StoneThrower
 from automata.Scenario import scenario as automata
 from time import sleep
 import random
@@ -15,12 +17,21 @@ def main():
     height = 10
 
     c1 = Character(point=Point(1,1), ruleset=Handstander, team=2)
-    c3 = Character(point=Point(1,1), ruleset=Handstander, team=2)
-    c4 = Character(point=Point(1,1), ruleset=Handstander, team=2)
-    c5 = Character(point=Point(1,0), ruleset=Handstander, team=1)
-    c6 = Character(point=Point(1,0), ruleset=Handstander, team=1)
-    c7 = Character(point=Point(1,0), ruleset=Handstander, team=1)
-    chars = [c1,c3,c4,c5,c6,c7]
+    c2 = Character(point=Point(1,1), ruleset=TupperWasher, team=2)
+    c3 = Character(point=Point(1,1), ruleset=StoneThrower, team=2)
+    c4 = Character(point=Point(1,1), ruleset=Handstander, team=1)
+    c5 = Character(point=Point(1,1), ruleset=TupperWasher, team=1)
+    c6 = Character(point=Point(1,1), ruleset=StoneThrower, team=1)
+    # c4 = Character(point=Point(1,0), ruleset=Handstander, team=1)
+    # c5 = Character(point=Point(1,1), ruleset=Handstander, team=2)
+    # c6 = Character(point=Point(1,1), ruleset=Handstander, team=2)
+    # c7 = Character(point=Point(1,1), ruleset=Handstander, team=2)
+    # c8 = Character(point=Point(1,0), ruleset=Handstander, team=1)
+    # c9 = Character(point=Point(1,0), ruleset=Handstander, team=1)
+    # c10 = Character(point=Point(1,0), ruleset=Handstander, team=1)
+    # c11 = Character(point=Point(1,0), ruleset=Handstander, team=1)
+    # c12 = Character(point=Point(1,0), ruleset=Handstander, team=1)
+    chars = [c1,c2,c3,c4,c5,c6]
 
     for c in chars:
         automata.add_character(c)
@@ -30,7 +41,7 @@ def main():
         automata.renderer.show()
 
         for c in chars:
-            print('Team: {} | Health: {}'.format(c.team, c.hp))
+            print('Team: {} | Ruleset: {} | Health: {}'.format(c.team, c.hp))
         automata.tick()
         print('\n\n')
         sleep(0.1)
